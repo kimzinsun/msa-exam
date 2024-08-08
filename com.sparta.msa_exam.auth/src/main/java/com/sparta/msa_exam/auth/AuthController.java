ff24a021b6b45cd99bfa00c57cc8b30cb228c345
Kim JinSeon <kwlstjs00@gmail.com>
2024-08-08 14:13:22 +0900

    feat: 응답에 server port header 추가

--- com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/AuthController.java
+++ com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/AuthController.java
@@ -3,26 +3,60 @@ package com.sparta.msa_exam.auth;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
-@AllArgsConstructor
 public class AuthController {
+
     private final AuthService authService;
+
+    @Value("${server.port}")
+    String port;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
 
     @PostMapping("/auth/signUp")
     public ResponseEntity<?> signUp(@RequestBody SignUpReqDto request) {
-        User user = new User(request.getUserId(), request.getUsername(), request.getPassword(), request.getRole());
-        return ResponseEntity.ok(authService.signUp(user));
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("ServerPort", port);
+
+        try {
+            User user = authService.signUp(request);
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .body(user);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest()
+                    .headers(headers)
+                    .body(e.getMessage());
+        }
 
     }
 
-    @PostMapping("/auth/signIn")
-    public ResponseEntity<?> signIn(@RequestBody SignInRequest user) {
-        return ResponseEntity.ok(new AuthResponse(authService.signIn(user.getUserId(), user.getPassword())));
+    @PostMapping("auth/signIn")
+    public ResponseEntity<?> signIn(@RequestBody SignInRequest request) {
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("ServerPort", port);
+
+            try {
+                String accessToken = authService.signIn(request.getUserId(), request.getPassword());
+                return ResponseEntity.ok()
+                        .headers(headers)
+                        .body(new AuthResponse(accessToken));
+            } catch (IllegalArgumentException e) {
+                return ResponseEntity.badRequest()
+                        .headers(headers)
+                        .body(e.getMessage());
+            }
     }
 
 
--- com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/AuthService.java
+++ com.sparta.msa_exam.auth/src/main/java/com/sparta/msa_exam/auth/AuthService.java
@@ -2,20 +2,23 @@ package com.sparta.msa_exam.auth;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.Optional;
 
+@Slf4j
 @Service
 public class AuthService {
     private final AuthRepository authRepository;
 
     private final PasswordEncoder passwordEncoder;
 
-    @Value("${service.jwt.secret}")
+    @Value("${jwt.secret}")
     String secretKey;
 
     @Value("${spring.application.name}")
@@ -29,20 +32,30 @@ public class AuthService {
         this.passwordEncoder = passwordEncoder;
     }
 
-    public ResponseEntity<?> signUp(User user) {
+    public User signUp(SignUpReqDto request) {
+        User user = new User(request.getUserId(), request.getUsername(), request.getPassword(), request.getRole());
+        Optional<User> checkUser = authRepository.findById(user.getUserId());
+        if(checkUser.isPresent()) {
+            throw new IllegalArgumentException("이미 존재하는 사용자입니다.");
+        }
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return ResponseEntity.ok(authRepository.save(user));
+
+        return authRepository.save(user);
     }
 
     public String signIn(String userId, String password) {
-        User user = authRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+        User user = authRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+
         if (!passwordEncoder.matches(password, user.getPassword())) {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
         }
+
         return createAccessToken(user.getUserId(), user.getRole());
     }
 
     public String createAccessToken(String userId, String role) {
+
         return Jwts.builder()
                 .claim("user_id", userId)
                 .claim("role", role)
